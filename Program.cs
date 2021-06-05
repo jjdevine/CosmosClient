8d60e69dd1cc2b3b48e06c8c7a4e1b0529a4e47f
Jonathan Devine <Jonathan.Devine@mhr.co.uk>
2021-06-05 20:02:18 +0100

    New Repo

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using Microsoft.Azure.Cosmos;
 
-namespace CosmosClient
+namespace TestCosmosClient
 {
     class Program
     {
+        private string EndpointUrl = "https://jd-cosmos.documents.azure.com:443/";
+
+        private string PrimaryKey = "EfoKRkQYFgYZRXG6DDwkgrB4yMVFU1ngGo4OgguKAZEecIB3OsMzQ2sCYuHsKBuSfIjGBBKVhzI30kLqBOfAQQ==";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CosmosClient cosmosClient = new CosmosClient(
+                "AccountEndpoint=https://jd-cosmos.documents.azure.com:443/;AccountKey=EfoKRkQYFgYZRXG6DDwkgrB4yMVFU1ngGo4OgguKAZEecIB3OsMzQ2sCYuHsKBuSfIjGBBKVhzI30kLqBOfAQQ==;",
+                new CosmosClientOptions()
+                {       
+                }
+            );
+
+            Database db = cosmosClient.GetDatabase("cosmos1");
+            Container container = db.GetContainer("container1");
+
+            var sqlQueryText = "SELECT * FROM c WHERE c.val = 'a'";
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator feedIterator = container.GetItemQueryIterator(queryDefinition);
+
+
+
         }
     }
 }
